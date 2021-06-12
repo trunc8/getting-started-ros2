5c908af5cdc1bd0f7eb12b50336848c05f254256
Siddharth Saha <sahasiddharth611@gmail.com>
2021-06-12 11:31:46 +0530

    Added ros_tutorial, my_package: first package, py_pubsub: Publisher and subscriber, py_srvcli: Server and Client

---
+++ src/py_srvcli/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'py_srvcli'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Siddharth Saha',
+    maintainer_email='sahasiddharth611@outlook.com',
+    description='Python client server tutorial',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)

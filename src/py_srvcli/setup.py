3a485a4d0a42a8cb69a4625742507033f6c82d2e
Siddharth Saha <sahasiddharth611@gmail.com>
2021-06-13 14:01:55 +0530

    Completed service and client tutorial

--- src/py_srvcli/setup.py
+++ src/py_srvcli/setup.py
@@ -20,6 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'service = py_srvcli.service_member_function:main',
+            'client = py_srvcli.client_member_function:main',
         ],
     },
 )
